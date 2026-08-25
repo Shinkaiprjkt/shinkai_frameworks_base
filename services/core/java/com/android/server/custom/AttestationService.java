7ac75104309858a40364e6efd177709bc7848119
kylieeXD <khayloaftck@gmail.com>
2026-08-25 09:09:17 +0000

    Revert "core: Make PIF user configurable [1/2]"
    This reverts commit a9b69a06642ceaa4b86b8aa64e5c7405ded7732f.
    
    Change-Id: I6a487dede2d89e6c3216c2417ecdb5b1576a5855
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

--- services/core/java/com/android/server/custom/AttestationService.java
+++ services/core/java/com/android/server/custom/AttestationService.java
@@ -14,7 +14,6 @@ import android.net.Network;
 import android.net.NetworkCapabilities;
 import android.os.Environment;
 import android.os.SystemProperties;
-import android.provider.Settings;
 import android.util.Log;
 
 import com.android.server.SystemService;
@@ -48,11 +47,13 @@ public final class AttestationService extends SystemService {
             "persist.sys.pihooks.disable.gms_props", false);
 
     private final Context mContext;
+    private final File mDataFile;
     private final ScheduledExecutorService mScheduler;
 
     public AttestationService(Context context) {
         super(context);
         mContext = context;
+        mDataFile = new File(Environment.getDataSystemDirectory(), DATA_FILE);
         mScheduler = Executors.newSingleThreadScheduledExecutor();
     }
 
@@ -67,6 +68,33 @@ public final class AttestationService extends SystemService {
             Log.i(TAG, "Scheduling the service");
             mScheduler.scheduleAtFixedRate(
                     new FetchGmsCertifiedProps(), INITIAL_DELAY, INTERVAL, TimeUnit.MINUTES);
+        }
+    }
+
+    private String readFromFile(File file) {
+        StringBuilder content = new StringBuilder();
+
+        if (file.exists()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    content.append(line);
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "Error reading from file", e);
+            }
+        }
+        return content.toString();
+    }
+
+    private void writeToFile(File file, String data) {
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(data);
+            // Set -rw-r--r-- (644) permission to make it readable by others.
+            file.setReadable(true, false);
+        } catch (IOException e) {
+            Log.e(TAG, "Error writing to file", e);
         }
     }
 
@@ -127,12 +155,12 @@ public final class AttestationService extends SystemService {
                     return;
                 }
 
-                String savedProps = Settings.Secure.getString(mContext.getContentResolver(), Settings.Secure.FETCHED_PIF);
+                String savedProps = readFromFile(mDataFile);
                 String props = fetchProps();
 
                 if (props != null && !savedProps.equals(props)) {
                     dlog("Found new props");
-                    Settings.Secure.putString(mContext.getContentResolver(), Settings.Secure.FETCHED_PIF, props);
+                    writeToFile(mDataFile, props);
                     dlog("FetchGmsCertifiedProps completed");
                 } else {
                     dlog("No change in props");
