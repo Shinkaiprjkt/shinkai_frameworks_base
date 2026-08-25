59e7d9870087e3697305c6dfe841cb6dca4d058e
kylieeXD <khayloaftck@gmail.com>
2026-08-25 08:58:59 +0000

    Revert "config: Make PIF update URL adhere PixelOS version"
    This reverts commit f478fb1f1f7acac0ffbf328e3dfd571e7a308bc0.
    
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

--- services/core/java/com/android/server/custom/AttestationService.java
+++ services/core/java/com/android/server/custom/AttestationService.java
@@ -37,9 +37,7 @@ import java.util.concurrent.TimeUnit;
 public final class AttestationService extends SystemService {
     private static final String TAG = AttestationService.class.getSimpleName();
     private static final String API =
-            Resources.getSystem()
-                    .getString(com.android.internal.R.string.config_pifUpdateUrl)
-                    .replace("{version}", SystemProperties.get("net.pixelos.version"));
+            Resources.getSystem().getString(com.android.internal.R.string.config_pifUpdateUrl);
 
     private static final String DATA_FILE = "gms_certified_props.json";
 
